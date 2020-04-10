c894fa42ec70e44c67888c44a515cb7909c8308c
kazumkob <kazumkob@yahoo-corp.jp>
2020-04-10 12:31:26 +0900

    first commit

---
+++ README_EN.md
@@ -0,0 +1,61 @@
+--------------------------------
+[Version]
+--------------------------------
+v1
+
+■ Change logs
+-----------
+
+
+--------------------------------
+[Overview]
+--------------------------------
+These code samples describe how to use PHP to call APIs.
+
+--------------------------------
+[Contents]
+--------------------------------
+conf/
+  - api_config.ini      : Config files to specify IDs.
+
+src/jp/co/yahoo/adssearchapi
+  - sample/             : Examples of Report services.
+
+--------------------------------
+[Development environment]
+--------------------------------
+To construct PHP environment, install followings.
+
+1. PHP 7.1.x or above
+2. Composer 1.9.3 or above
+3. OpenAPI generator 4.2.3 or above
+4. Write Ids in conf/api_config.ini like this.
+  - accountId          : API account ID (required)
+  - accessToken        : API accessToken (required)
+
+--------------------------------
+[How to execute Sample Code]
+--------------------------------
+Move to the directory where you stored the cloned sample program, and execute OpenAPI Generator to generate a PHP client.
+```
+openapi-generator generate -i https://yahoojp-marketing.github.io/ads-search-api-documents/design/v1/Route.yaml -g php -o ./
+```
+
+Then, composer.json is generated immediately below, so execute client install.
+```
+composer install
+```
+
+Execution example
+```
+php ./src/jp/co/yahoo/adssearchapi/adssearchapi/sample/ReportDefinitionServiceSample.php
+```
+
+--------------------------------
+NOTICE：　Yahoo! Ads Search Ads API - For use of sample code
+--------------------------------
+
+
+The sample code of Yahoo! Ads API is provided to API users only who concluded the contract of "Application to Use Yahoo! Promotional Ads API" with Yahoo Japan Corporation.
+
+Additionally, please note that Yahoo Japan Corporation may change the contents and the specification of the sample code, and may discontinue providing the sample code without any notice.
---
+++ src/jp/co/yahoo/adssearchapi/sample/ReportDefinitionServiceSample.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Copyright (C) 2020 Yahoo Japan Corporation. All Rights Reserved.
+ */
+
+namespace jp\co\yahoo\adssearchapi\sample;
+
+require_once __DIR__ . '/../../../../../../vendor/autoload.php';
+
+use Exception;
+use GuzzleHttp\Client;
+use OpenAPI\Client\Configuration;
+use OpenAPI\Client\Api\ReportDefinitionServiceApi;
+use OpenAPI\Client\Model\{
+    ReportDefinition,
+    ReportDefinitionServiceOperation,
+    ReportDefinitionServiceReportDateRangeType,
+    ReportDefinitionServiceReportDownloadEncode,
+    ReportDefinitionServiceReportDownloadFormat,
+    ReportDefinitionServiceReportType,
+    ReportDefinitionServiceSelector,
+    ReportDefinitionServiceReportJobStatus,
+    ReportDefinitionServiceDownloadSelector
+};
+
+
+/**
+ * example ReportDefinitionService operation and Utility method collection.
+ */
+class ReportDefinitionServiceSample
+{
+
+    private static $apiConfig = null;
+
+    /**
+     * example report fields.
+     */
+    const REPORT_FIELDS = [
+        'ACCOUNT_ID',
+        'IMPS',
+        'CLICKS'
+    ];
+
+    /**
+     * ReportDefinitionServiceSample constructor.
+     */
+    public static function init(): void
+    {
+        self::$apiConfig = parse_ini_file(__DIR__ . '/../../../../../../conf/api_config.ini');
+
+    }
+
+    /**
+     * example ReportDefinitionService operation.
+     * @throws Exception
+     */
+    public static function runExample(): void
+    {
+        self::init();
+
+        $config = Configuration::getDefaultConfiguration()->setAccessToken(self::$apiConfig['accessToken']);
+
+        $apiInstance = new ReportDefinitionServiceApi(
+            new Client(),
+            $config
+        );
+        $job_id = null;
+
+        // Add
+        $operand = new ReportDefinition();
+        $operand->setAccountId(self::$apiConfig['accountId']);
+        $operand->setReportName("REPORT_SAMPLE");
+        $operand->setReportType(ReportDefinitionServiceReportType::ACCOUNT);
+        $operand->setFields(self::REPORT_FIELDS);
+        $operand->setReportDateRangeType(ReportDefinitionServiceReportDateRangeType::YESTERDAY);
+        $operand->setReportDownloadEncode(ReportDefinitionServiceReportDownloadEncode::UTF_8);
+        $operand->setReportDownloadFormat(ReportDefinitionServiceReportDownloadFormat::CSV);
+
+        $report_definition_service_operation = new ReportDefinitionServiceOperation();
+        $report_definition_service_operation->setAccountId(self::$apiConfig['accountId']);
+        $report_definition_service_operation->setOperand([$operand]);
+
+        try {
+            $result = $apiInstance->reportDefinitionServiceAddPost($report_definition_service_operation);
+
+            $job_id = $result->getRval()->getValues()[0]->getReportDefinition()->getReportJobId();
+
+        } catch (Exception $e) {
+            echo 'Exception when calling ReportDefinitionServiceApi->reportDefinitionServiceAddPost: ', $e->getMessage(), PHP_EOL;
+        }
+
+        // Get
+        $report_definition_service_selector = new ReportDefinitionServiceSelector();
+        $report_definition_service_selector->setReportJobIds([$job_id]);
+        $report_definition_service_selector->setAccountId(self::$apiConfig['accountId']);
+
+        try {
+            $result = $apiInstance->reportDefinitionServiceGetPost($report_definition_service_selector);
+            $job_status = $result->getRval()->getValues()[0]->getReportDefinition()->getReportJobStatus();
+
+            while($job_status != ReportDefinitionServiceReportJobStatus::COMPLETED){
+                sleep(1);
+                $result = $apiInstance->reportDefinitionServiceGetPost($report_definition_service_selector);
+                $job_status = $result->getRval()->getValues()[0]->getReportDefinition()->getReportJobStatus();
+            }
+
+        } catch (Exception $e) {
+            echo 'Exception when calling ReportDefinitionServiceApi->reportDefinitionServiceGetPost: ', $e->getMessage(), PHP_EOL;
+        }
+
+        // Download
+        $report_definition_service_download_selector = new ReportDefinitionServiceDownloadSelector();
+        $report_definition_service_download_selector->setAccountId(self::$apiConfig['accountId']);
+        $report_definition_service_download_selector->setReportJobId($job_id);
+
+        try {
+            $data = $apiInstance->reportDefinitionServiceDownloadPost($report_definition_service_download_selector);
+            file_put_contents('./download/'.'sample.csv',$data);
+        } catch (Exception $e) {
+            echo 'Exception when calling ReportDefinitionServiceApi->reportDefinitionServiceDownloadPost: ', $e->getMessage(), PHP_EOL;
+        }
+
+    }
+
+}
+
+if (__FILE__ != realpath($_SERVER['PHP_SELF'])) {
+    return;
+}
+
+try {
+    ReportDefinitionServiceSample::runExample();
+} catch (Exception $e) {
+    print $e->getMessage();
+}
